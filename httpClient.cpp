a77c75d60ca0ec9ad3a924e3f13c77c3f702cb0c
Niran_Desktop <niranshivu@gmail.com>
2025-08-01 17:51:20 +0800

    Removed custom HTTP client and use libcurl for easier transition to getting API response from HTTPS clients

--- httpClient.cpp
+++ httpClient.cpp
@@ -1,104 +1,18 @@
 #include "httpClient.hpp"
 
-//#pragma comment(lib, "Ws2_32.lib") //Linking instructions for Visual C++, will not work for g++. Link manually instead 
-//-lws2_32 flag added to g++
-
-
-    HttpClient::HttpClient(HttpClient::REQUEST_TYPE requestType){
-            //Initialise WinSocksDLL
-            this -> iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
-            if(this -> iResult != 0){
-                std::cout << "WinSock failed to start" << "\n";
-            }
-            this -> port = DEFAULT_PORT;
-            this -> result=NULL;
-            this -> ptr = NULL;
-            this -> hints = {};
-            this -> requestType = requestType;   
-            this -> hints.ai_family = AF_UNSPEC;
-            this -> hints.ai_socktype = SOCK_STREAM;
-            this -> hints.ai_protocol = IPPROTO_TCP;
-
-    };
-    void HttpClient::createSocket(){
-        std::cout << "Resolving ip address from hostname "<<  this -> hostname  << " on port " << this -> port << "\n";
-        
-        iResult = getaddrinfo(this -> hostname, this -> port, &hints, &result);
-        if(iResult != 0){
-            std::cout << "Requested Server failed. Error Code:  " << iResult << "\t Error Description: "<< gai_strerror(iResult) << "\n";
-            WSACleanup();
-            return;
-        }
-        this -> ptr = this -> result;
-        currSocket = INVALID_SOCKET;
-        //Initialise socket object from the response we receive from DNS resolution of IP address from HOSTNAME 
-        std::cout << "Initialising Socket" << "\n";
-        currSocket = socket(this -> ptr -> ai_family,this -> ptr -> ai_socktype, this -> ptr -> ai_protocol);
-        if (currSocket == INVALID_SOCKET) 
-            {
-                std::cout << "Error at socket(): " <<  WSAGetLastError() << "\n";
-                freeaddrinfo(this -> ptr);
-                WSACleanup();
-                return;
-            }
-
-        iResult = connect(currSocket,this -> ptr -> ai_addr,(int)this -> ptr -> ai_addrlen); 
-
-        if(iResult == SOCKET_ERROR)
-            {
-                std::cout << "Socket Error" << "\n";
-                closesocket(currSocket);
-                currSocket = INVALID_SOCKET;
-                return;
-            }
-        return;
-    }
-    void HttpClient::request(std::string query)
-    {
-        char *buffer = (char*)std::calloc(DEFAULT_BUFLEN,sizeof(char));
-        std::string requestType;
-        //const char* httpRequest = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n";
-        if(this -> requestType == REQUEST_TYPE::GET){
-            requestType = "GET ";  
-        }
-        std::string host = this -> hostname;
-        std::string httpRequest = requestType + "/" + query + " HTTP/1.1\r\n" + "Host: " + host +"\r\n\r\n"; 
-        std::cout << httpRequest << "\n";
-        send(currSocket, httpRequest.c_str(), strlen(httpRequest.c_str()), 0);
-        iResult = recv(currSocket,buffer,DEFAULT_BUFLEN,0);
-        if(iResult > 0 ){
-                std::cout << "Bytes received: " << iResult << "\n";
-            }
-            else if(iResult == 0){
-                std::cout << "no data received" << "\n";
-            }
-            else{
-                std::cout << "recv failed : " << WSAGetLastError() << "\n";
-            }
-        freeaddrinfo(this -> ptr);
-        free(buffer);
-
-        if (currSocket == INVALID_SOCKET) {
-            std::cout << "Unable to connect to server!" << "\n";
-            closesocket(currSocket);
-            WSACleanup();
-            return;
-        }
-
-        };
-
 
 int main(){
-    enum HttpClient::REQUEST_TYPE requestType= HttpClient::REQUEST_TYPE::GET;
-    PCSTR hostname = "youtube.com";
-    HttpClient client (requestType);
-    client.hostname = hostname;
-    
-    std::cout << "HTTP Client Created" << "\n";  
-
-    client.createSocket();
-    std::string a = ""; 
-    client.request(a);
-  
+    CURLcode globalHandleRet = curl_global_init(CURL_GLOBAL_ALL); //Global initialisation for CURL
+    CURL * curl = curl_easy_init(); //Create CURL easy handle
+    if(curl) {
+        CURLcode res;
+        std::cout << "Success" << "\n";
+        curl_easy_setopt(curl, CURLOPT_URL, "https://google.com");
+        res = curl_easy_perform(curl);
+        std::cout << res << "\n";
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
     return 0;
+    
 }
--- httpClient.hpp
+++ httpClient.hpp
@@ -4,50 +4,10 @@
 
 #ifndef HTTPCLIENT // include guard
 #define HTTPCLIENT
+#include <curl/curl.h>
 #include <iostream>
-#include <string>
-#include <windows.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <iphlpapi.h>
-
-#define DEFAULT_BUFLEN 8192 //Default buffer length of 8KB
-
-constexpr PCSTR DEFAULT_PORT = "443";
 
 
-
-class HttpClient {
-    public:
-   
-        enum REQUEST_TYPE {
-            GET,
-            POST,
-            PUT,
-            PATCH,
-            DEL
-
-        };
-        
-        REQUEST_TYPE requestType;
-        PCSTR hostname;
-        PCSTR port;
-
-        int iResult;
-
-        WSADATA wsaData;
-
-        //ip address info struct initialised to hold information from transactions
-        addrinfo *result;
-        addrinfo *ptr;
-        addrinfo hints;
-
-        SOCKET currSocket;
-        HttpClient(REQUEST_TYPE requestType);
-        void createSocket();
-        void request(std::string query);
-
-};
 
 
 #endif /* HTTPCLIENT */
